24c73f9865610c18d5c10f8bbb79816e6a5096d5
马亚超 <1780116958@qq.com>
2019-08-02 18:41:21 +0800

    ce

---
+++ ce/src/main/java/com/bawie/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.bawie.mapper;
+
+import com.bawie.entity.User;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface UserMapper extends Mapper<User> {
+
+}
---
+++ ce/src/main/java/com/bawie/service/UserService.java
@@ -0,0 +1,10 @@
+package com.bawie.service;
+
+import com.bawie.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+}
